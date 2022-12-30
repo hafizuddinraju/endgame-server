563e03b3286cdd8750091d9a4616cb5df6c0304b
hafizuddinraju <hafizraju.cse.bubt@gmail.com>
2022-12-30 23:49:54 +0600

    server site

---
+++ models/post.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    postContent: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    postLikes: {
+        type: Number,
+    }
+});
+
+// postSchema.methods.addComment = async function (postId) {
+//     try {
+//         const postIndex = this.comments.findIndex(
+//             (post) => post.comment.toString() === postId.toString()
+//         );
+//         this.comments.push({
+//             comment: postId,
+//         });
+
+//         await this.save();
+//     } catch (error) {
+//         console.log(error);
+//         throw new Error(error);
+//     }
+// };
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
